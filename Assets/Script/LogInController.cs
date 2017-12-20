c32254ea8d8f247dcd836f8f74f6f6f34ac60994
Nohelya Dos Santos <nohe@Nohelyas-MacBook-Pro.local>
2017-12-20 08:34:25 -0600

    prueba visionaria

---
+++ Assets/Script/LogInController.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Firebase;
+using Firebase.Database;
+using Firebase.Unity.Editor;
+
+
+public class LogInController : MonoBehaviour {
+
+	public InputField user;
+	public InputField pwdF;
+	public Button registerbutton;
+
+	private string userString;
+	private string pwdString;
+	DatabaseReference reference;
+	// Use this for initialization
+
+
+	void Start () {
+
+		// Set up the Editor before calling into the realtime database.
+		FirebaseApp.DefaultInstance.SetEditorDatabaseUrl("https://pong-574e7.firebaseio.com/");
+
+		// Get the root reference location of the database.
+		 reference = FirebaseDatabase.DefaultInstance.RootReference;
+
+		Button btn = registerbutton.GetComponent<Button>();
+
+		btn.onClick.AddListener(delegate{CheckDB(userString,pwdString);});
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		userString = user.text;
+		pwdString = pwdF.text;
+		Debug.Log (userString);
+		Debug.Log (pwdString);
+
+
+	}
+
+
+	public void CheckDB( string user,string pwd) {
+
+		FirebaseDatabase.DefaultInstance
+			.GetReference("users")
+			.GetValueAsync().ContinueWith(task => {
+				if (task.IsFaulted) {
+					Debug.Log("Hubo un error");
+					// Handle the error...
+				}
+				else if (task.IsCompleted) {
+					DataSnapshot snapshot = task.Result;
+					Debug.Log(snapshot);
+
+					if (snapshot != null && snapshot.ChildrenCount > 0) {
+
+						foreach (var childSnapshot in snapshot.Children) {
+							var name = childSnapshot.Child ("name").Value.ToString (); 
+
+							//text.text = name.ToString();
+							Debug.Log(name.ToString());
+
+
+						}
+					}
+					}
+			});
+		Debug.Log("se ejecuto la query");
+
+		}
+}
---
+++ Assets/Script/RegisterController.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Firebase;
+using Firebase.Database;
+using Firebase.Unity.Editor;
+
+
+public class User {
+	public string username;
+	public string pwd;
+
+	public User() {
+	}
+
+	public User(string username, string pwd) {
+		this.username = username;
+		this.pwd = pwd;
+	}
+
+}
+
+public class RegisterController : MonoBehaviour {
+
+	public InputField user;
+	public InputField paswd;
+	public Button registerbutton;
+
+	private string userString;
+	private string pwdString;
+	DatabaseReference reference;
+	// Use this for initialization
+
+
+	void Start () {
+
+		// Set up the Editor before calling into the realtime database.
+		FirebaseApp.DefaultInstance.SetEditorDatabaseUrl("https://pong-574e7.firebaseio.com/");
+
+		// Get the root reference location of the database.
+		 reference = FirebaseDatabase.DefaultInstance.RootReference;
+
+		Button btn = registerbutton.GetComponent<Button>();
+
+		btn.onClick.AddListener(delegate{writeNewUser(userString,userString,pwdString);});
+
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		userString = user.text;
+		pwdString = paswd.text;
+
+		Debug.Log (userString);
+		Debug.Log (pwdString);
+
+	}
+
+	public void LoadScene(int level)
+	{
+		
+		Application.LoadLevel(level);
+	}
+
+	public void writeNewUser(string userId, string name, string password) {
+		User user = new User(name, password);
+		string json = JsonUtility.ToJson(user);
+
+		Debug.Log (name);
+		Debug.Log (password);
+
+		reference.Child("users").Child(userId).SetRawJsonValueAsync(json);
+		Debug.Log("se ejecuto la query");
+
+	}
+}
